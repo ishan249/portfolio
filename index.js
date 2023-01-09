129e044997da839b1e006f61ccab1e9b6bc0fa11
ishan249 <249patelishan@gmail.com>
2023-01-09 23:24:27 +0530

    navbar changes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -17,33 +19,46 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-function goTohashNode(){
+function goTohashNode() {
+  window.open("https://ishanpatel.hashnode.dev/", "_blank");
+}
+function goTohashNodeHotelBlog() {
   window.open(
-             "https://ishanpatel.hashnode.dev/", "_blank");
+    "https://ishanpatel.hashnode.dev/hotel-booking-system-with-express-js",
+    "_blank"
+  );
 }
-function goTohashNodeHotelBlog(){
+function goTohashHotelSite() {
+  window.open("https://hotel-booking-pi.vercel.app/", "_blank");
+}
+function goTohashBillSite() {
+  window.open("https://ishan249.github.io/Billnotes/", "_blank");
+}
+function goTohashBillBlog() {
+  window.open("https://ishanpatel.hashnode.dev/learn-javascript-dom", "_blank");
+}
+function goToResume() {
   window.open(
-             "https://ishanpatel.hashnode.dev/hotel-booking-system-with-express-js", "_blank");
+    "https://drive.google.com/file/d/1MTdSt9EPfrXqpvMAAKVVfix7uc9w4L7j/view?usp=sharing",
+    "_blank"
+  );
 }
-function goTohashHotelSite(){
+function goToRedditSite() {
+  window.open("https://best-of-reddit.vercel.app/", "_blank");
+}
+function goToRedditHashBlog() {
   window.open(
-             "https://hotel-booking-pi.vercel.app/", "_blank");
+    "https://ishanpatel.hashnode.dev/web-scraping-using-cheerio-in-nodejs",
+    "_blank"
+  );
 }
-function goTohashBillSite(){
-  window.open(
-             "https://ishan249.github.io/Billnotes/", "_blank");
-}
-function goTohashBillBlog(){
-  window.open(
-             "https://ishanpatel.hashnode.dev/learn-javascript-dom", "_blank");
-}
-function goToResume(){
-  window.open("https://drive.google.com/file/d/1MTdSt9EPfrXqpvMAAKVVfix7uc9w4L7j/view?usp=sharing","_blank");
-}
-function goToRedditSite(){
-  window.open("https://best-of-reddit.vercel.app/","_blank");
-}
-function goToRedditHashBlog(){
-  window.open("https://ishanpatel.hashnode.dev/web-scraping-using-cheerio-in-nodejs","_blank");
 
+function classToggle() {
+  const navs = document.querySelectorAll(".Navbar__Items");
+
+  navs.forEach((nav) => nav.classList.toggle("Navbar__ToggleShow"));
 }
+
+document
+  .querySelector(".Navbar__Link-toggle")
+  .addEventListener("click", classToggle);
